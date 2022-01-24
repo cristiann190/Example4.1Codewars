22923767ed515c694d699344f91f2161af34d3d8
cristiann190 <cristiann190@gmail.com>
2022-01-24 21:17:16 +0100

    Convert a number to the roman value.

--- src/src/Conversion.java
+++ src/src/Conversion.java
@@ -1,6 +1,9 @@
 package src;
 
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Map;
+import java.util.TreeMap;
 
 public class Conversion {
 
@@ -79,5 +82,38 @@ public class Conversion {
                 return unitValue + maxValue;
         }
         return "";
+    }
+
+    ///////////////////////////////////////BEST OPTIMIZED EXAMPLE////////////////////////////////////////////////
+
+    private static TreeMap<Integer, String> MAP;
+    static {
+        MAP = new TreeMap<Integer, String>(Collections.reverseOrder());
+        MAP.put( 1, "I" );
+        MAP.put( 4, "IV" );
+        MAP.put( 5, "V" );
+        MAP.put( 9, "IX" );
+        MAP.put( 10, "X" );
+        MAP.put( 40, "XL" );
+        MAP.put( 50, "L" );
+        MAP.put( 90, "XC" );
+        MAP.put( 100, "C" );
+        MAP.put( 400, "CD" );
+        MAP.put( 500, "D" );
+        MAP.put( 900, "CM" );
+        MAP.put( 1000, "M" );
+    }
+
+    public String solutionOptimized(int n) {
+        StringBuilder builder = new StringBuilder();
+        for( Map.Entry<Integer, String> entry: MAP.entrySet() ){
+            int i = entry.getKey();
+            String s = entry.getValue();
+            while( n>=i ){
+                builder.append(s);
+                n -= i;
+            }
+        }
+        return builder.toString();
     }
 }
--- test/src/ConversionTest.java
+++ test/src/ConversionTest.java
@@ -10,7 +10,7 @@ import java.util.TreeMap;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class ConversionTest {
-    private Conversion conversion = new Conversion();
+    private final Conversion conversion = new Conversion();
 
     @Test
     public void shouldConvertToRoman() {
@@ -19,37 +19,6 @@ class ConversionTest {
         assertEquals("VI", conversion.solution(6), "solution(6) should equal to VI");
         assertEquals("MIX", conversion.solution(1009), "solution(6) should equal to VI");
         assertEquals("MLXVI", conversion.solution(1066), "solution(6) should equal to VI");
-        assertEquals("MDCCLXXVI", solution(1776), "solution(6) should equal to VI");
-    }
-
-    private static TreeMap<Integer, String> MAP;
-    static {
-        MAP = new TreeMap<Integer, String>(Collections.reverseOrder());
-        MAP.put( 1, "I" );
-        MAP.put( 4, "IV" );
-        MAP.put( 5, "V" );
-        MAP.put( 9, "IX" );
-        MAP.put( 10, "X" );
-        MAP.put( 40, "XL" );
-        MAP.put( 50, "L" );
-        MAP.put( 90, "XC" );
-        MAP.put( 100, "C" );
-        MAP.put( 400, "CD" );
-        MAP.put( 500, "D" );
-        MAP.put( 900, "CM" );
-        MAP.put( 1000, "M" );
-    }
-
-    public String solution(int n) {
-        StringBuilder builder = new StringBuilder();
-        for( Map.Entry<Integer, String> entry: MAP.entrySet() ){
-            int i = entry.getKey();
-            String s = entry.getValue();
-            while( n>=i ){
-                builder.append(s);
-                n -= i;
-            }
-        }
-        return builder.toString();
+        assertEquals("MDCCLXXVI", conversion.solution(1776), "solution(6) should equal to VI");
     }
 }
